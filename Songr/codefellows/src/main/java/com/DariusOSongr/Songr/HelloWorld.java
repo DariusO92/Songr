0228a8b4073f63d3b760397f9420b895e5304504
darius owens <darius.owens.j@gmail.com>
2022-06-01 20:13:05 -0700

    added new dependicenes and application properties

--- Songr/codefellows/src/main/java/com/DariusOSongr/Songr/HelloWorld.java
+++ Songr/codefellows/src/main/java/com/DariusOSongr/Songr/HelloWorld.java
@@ -2,6 +2,7 @@ package com.DariusOSongr.Songr;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,7 +11,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HelloWorld {
     @RequestMapping(value = "/" , method = RequestMethod.GET)
     public String getTest(){
-
+    return "splashPage";
+    }
+    @GetMapping("/hi")
+    public String getHi(){
+        return "hello";
     }
 
 }
